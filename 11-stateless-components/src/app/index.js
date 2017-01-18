60cacd89663bb9e22985685226ad37db21a328c8
Kenneth Schnall <kas6570@psu.edu>
2017-01-18 08:42:35 -0500

    Finish video 11

---
+++ 11-stateless-components/src/app/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom'
+
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
+class App extends React.Component {
+  render() {
+    return( // can only return 1 ROOT element (in this case we return 1 div)
+      <div>
+        <div className='row'>
+          <div className='small-12 columns'>
+            <Header homeLink='Home'/>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='small-12 columns'>
+            <Home name={'Ken'} initialAge={20}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById('app'));
